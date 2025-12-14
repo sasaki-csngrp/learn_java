682f5bb89525374350edd2d2135417e8e6eac9c4
sasaki-csngrp <sasaki@csngrp.co.jp>
2025-12-14 01:59:19 +0000

    feat: add initial implementation

---
+++ gof-design-patterns/srcs/P00_01 Encapsulation/BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+    private double balance;  // データをprivateで隠蔽
+    
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000);
+        account.deposit(500);
+        account.withdraw(200);
+        System.out.println("残高: " + account.getBalance());
+    }
+}
