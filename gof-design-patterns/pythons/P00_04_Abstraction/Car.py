28cadcc5260afc30fd731bcc89d2cc39c3ab5600
sasaki-csngrp <sasaki@csngrp.co.jp>
2025-12-20 02:32:14 +0000

    feat: Python のサンプルを若干修正

--- gof-design-patterns/pythons/P00_04_Abstraction/Car.py
+++ gof-design-patterns/pythons/P00_04_Abstraction/Car.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 # 抽象クラス
 class Vehicle(ABC):
@@ -24,20 +25,33 @@ class Vehicle(ABC):
 
 # 具象クラス
 class Car(Vehicle):
+    @override
     def __init__(self, brand: str):
         super().__init__(brand)  # 親クラスのコンストラクタを呼び出す
     
+    @override
     def start(self) -> None:
         """エンジンを始動する（実装）"""
         print(f"{self._brand}の車がエンジンを始動しました")
     
+    @override
     def stop(self) -> None:
         """停止する（実装）"""
         print(f"{self._brand}の車が停止しました")
 
 
 if __name__ == "__main__":
-    car = Car("Toyota")
+    vehicle: Vehicle = Car("Toyota")
+    vehicle.start()
+    vehicle.stop()
+    vehicle.display_brand()
+
+    car: Car = Car("Honda")
     car.start()
     car.stop()
     car.display_brand()
+
+    vehicle = car
+    vehicle.start()
+    vehicle.stop()
+    vehicle.display_brand()
